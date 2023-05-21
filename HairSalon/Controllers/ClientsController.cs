2783bac8377536a4dd4987bc8514aafd99012594
Billy Lee <codingbillylee@gmail.com>
2023-05-21 01:46:43 -0700

    Add create function and view client function

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -16,7 +16,24 @@ namespace HairSalon.Controllers
       _db = db;
     }
 
-    
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 
 }
